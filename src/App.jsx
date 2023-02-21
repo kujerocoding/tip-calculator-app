0d92abb4eaa6ff6d6614fec1cacd47283a59a616
herom <jerome.manset22@gmail.com>
2023-02-22 01:34:11 +0800

    add elements to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='main--container'>
+      <h1>Splitter</h1>
+      <div className='app--container'>
+        <form>
+          <label>Bill</label>
+          <input type="text" />
+          <label htmlFor="">Select Tip %</label>
+          <div>
+            <p>5%</p>
+            <p>10%</p>
+            <p>15%</p>
+            <p>25%</p>
+            <p>50%</p>
+            <p>Custom</p>
+          </div>
+          <label htmlFor="">Number of People</label>
+          <input type="number" />
+        </form>
+        <div className='result--container'>
+          <p>Tip amount / person -$$$-</p>
+          <p>Total / person -$$$-</p>
+          <button>Reset</button>
+        </div>
+      </div>
     </div>
   )
 }
