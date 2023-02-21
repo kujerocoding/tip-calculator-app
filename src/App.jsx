97aef0563fee073736b0f1d6a8f76bc76d08e537
herom <jerome.manset22@gmail.com>
2023-02-22 03:46:07 +0800

    create state of object for billAmount, peopleCount and tipPercentage and create handleChange function to get the value from input change

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
+
+  const [formData, setFormData] = useState({
+    billAmount: null,
+    peopleCount: null,
+    tipPercentage: null
+  })
+
+  function handleChange(e){
+    const  {name, value } = e.target
+    setFormData(prevformData => {
+      return {...prevformData, [name]: value}
+    })
+  }
+
+
+console.log(formData)
+
   return (
     <div className='main--container'>
       <img src="./src/assets/images/logo.svg" alt="" />
       <div className='app--container'>
         <form>
           <label className="label--bill">Bill</label>
-          <input type="text" />
+          <input name="billAmount" type="text" onChange={handleChange}/>
           <fieldset>
             <legend>Select Tip %</legend>
             
-            <label><input id='five' type="radio" />5%</label>
-            <label><input id='ten' type="radio" />10%</label>
-            <label><input id='fifteen' type="radio" />15%</label>
-            <label><input id='twentyfive' type="radio" />25%</label>            
-            <label><input id='fifty' type="radio" />50%</label>
-            <label><input id='custom' type="radio" />Custom</label>
+            <label><input name="tipPercentage" value="5" id='five' type="radio" onChange={handleChange}/>5%</label>
+            <label><input name="tipPercentage" value="10" id='ten' type="radio" onChange={handleChange}/>10%</label>
+            <label><input name="tipPercentage" value="15" id='fifteen' type="radio" onChange={handleChange}/>15%</label>
+            <label><input name="tipPercentage" value="25" id='twentyfive' type="radio" onChange={handleChange}/>25%</label>            
+            <label><input name="tipPercentage" value="50" id='fifty' type="radio" onChange={handleChange}/>50%</label>
+            <label><input name="tipPercentage" value="" id='custom' type="radio" onChange={handleChange}/>Custom</label>
           </fieldset>
           <label className="label--number--people" htmlFor="">Number of People</label>
-          <input type="number" />
+          <input name="peopleCount" type="number" onChange={handleChange}/>
         </form>
         <div className='result--container'>
           <div  className='tiptotal--container'>
